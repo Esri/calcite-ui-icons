cd6f4d42c3dd068b6606f7ab82aca33baf640ab2
Paul C Pederson <paul.c.pederson@gmail.com>
2019-09-04 09:40:37 -0700

    add spaces back to path data (reverts #80)

--- js/compassNorthCircle16F.js
+++ js/compassNorthCircle16F.js
@@ -1,1 +1,1 @@
-export const compassNorthCircle16F = "M7.5 1A7.5 7.5 0 1015 8.5 7.509 7.509 0 007.5 1zm0 9l-4 2 4-9 4 9z";
+export const compassNorthCircle16F = "M7.5 1A7.5 7.5 0 1 0 15 8.5 7.509 7.509 0 0 0 7.5 1zm0 9l-4 2 4-9 4 9z";
--- js/graphScatterPlot16.js
+++ js/graphScatterPlot16.js
@@ -1,1 +1,1 @@
-export const graphScatterPlot16 = "M10.5 8.5a1 1 0 111 1 1 1 0 01-1-1zm2-4a1 1 0 10-1-1 1 1 0 001 1zm-6 3a1 1 0 10-1-1 1 1 0 001 1zm-2 4a1 1 0 10-1-1 1 1 0 001 1zM2 14V0H1v15h15v-1z";
+export const graphScatterPlot16 = "M10.5 8.5a1 1 0 1 1 1 1 1 1 0 0 1-1-1zm2-4a1 1 0 1 0-1-1 1 1 0 0 0 1 1zm-6 3a1 1 0 1 0-1-1 1 1 0 0 0 1 1zm-2 4a1 1 0 1 0-1-1 1 1 0 0 0 1 1zM2 14V0H1v15h15v-1z";
