23cafcb49442b2f20e43475e9f08240173e5302a
Nathan Castle <ncastle@esri.com>
2020-02-25 15:56:25 -0800

    Updates from review: size arg + asset catalogs

--- bin/convert-mobile.js
+++ bin/convert-mobile.js
@@ -6,9 +6,11 @@ const path = require('path');
 const yargs = require('yargs');
 
 const baseIconPath = './icons/';
-var sourceIconSuffix = '-16.svg';
+var sourceIconSuffix = '-32.svg';
 // default output path, can be overridden
 var outputRoot = path.join(__dirname, 'output')
+// default output size, can be overridden
+var outputSize = 64;
 
 /**
  * Converts a single svg to png, with given width & height values
@@ -52,7 +54,7 @@ function convertIconToXcodeImageSet(iconName, outputRoot) {
   convertSingleIconToPng(iconName, 96, 96, new_output_root, '@3x.png');
 
   // read template
-  const template_path = path.join(__dirname, 'templates', 'Contents.json');
+  const template_path = path.join(__dirname, 'templates', 'imageset.json');
 
   fs.readFile(template_path, 'utf8', function (error, buffer) {
     if (error){
@@ -66,17 +68,35 @@ function convertIconToXcodeImageSet(iconName, outputRoot) {
 }
 
 const options = yargs
-  .usage('Usage: -n <name of icon, omit if doing bulk>, -o <output path (defaults to ./output)>, -p <target platform (e.g. ios) -insize <16, 24, 32, omit for 16>')
+  .usage('Usage: -n <name of icon, omit if doing bulk>, \n-s <output size, pixels, default 64>, \n-o <output path (defaults to ./output)>, \n-p <target platform (e.g. ios) \n-insize <16, 24, 32, omit for 16>')
   .option('n', { alias: 'name', describe: 'name of icon, without -32.svg; omit to convert all icons', type: 'string', demandOption: false })
   .option('o', { alias: 'outputDir', describe: 'output path, relative to this script', type: 'string', demandOption: false })
   .option('p', { alias: 'outputPlatform', describe: 'target platform, valid options are: ios', type: 'string', demandOption: false })
   .option('insize', { alias: 'inSize', describe: 'source svg variant, defaults to 16', type: 'string', demandOption: false })
+  .option('s', {alias: 'outSize', describe: 'size of output image in pixels, doesn\'t apply when targeting specific platform', type: 'int', demandOption: false})
   .argv;
 
 if (options.outputDir) {
   outputRoot = path.join(__dirname, options.outputDir);
 }
 
+// update output size if set
+if (options.outSize){
+  outputSize = options.outSize;
+}
+
+// update source icon size based on output size if source not explicitly set
+if (!options.inSize && options.outSize){
+  if (options.outSize < 24){
+    sourceIconSuffix = '-16.svg';
+  } else if (options.outSize < 32){
+    sourceIconSuffix = '-24.svg';
+  } else {
+    sourceIconSuffix = '-32.svg';
+  }
+}
+
+// set insize if explicitly set
 if (options.inSize === '16'){
   sourceIconSuffix = '-16.svg';
 } else if (options.inSize === '24'){
@@ -85,13 +105,34 @@ if (options.inSize === '16'){
   sourceIconSuffix = '-32.svg';
 }
 
+if (options.outputPlatform === 'ios'){
+  // Put in .xcassets folder
+  outputRoot = path.join(outputRoot, 'calcite.xcassets');
+  
+  // Make sure dir exists
+  if (!fs.existsSync(outputRoot)) {
+    fs.mkdirSync(outputRoot, { recursive: true });
+  }
+
+  // read contents.json template
+  const template_path = path.join(__dirname, 'templates', 'xcassets.json');
+
+  // write out file
+  fs.readFile(template_path, 'utf8', function (error, buffer) {
+    if (error){
+      console.log(error);
+      process.exit(1);
+    }
+    const contents_output_path = path.join(outputRoot, 'Contents.json');
+    fs.writeFile(contents_output_path, buffer, function (error) { });
+  });
+}
+
 if (options.name) {
   if (options.outputPlatform === 'ios') {
     convertIconToXcodeImageSet(options.name, outputRoot);
   } else {
-    convertSingleIconToPng(options.name, 32, 32, outputRoot, '-32.png');
-    convertSingleIconToPng(options.name, 16, 16, outputRoot, '-16.png');
-    convertSingleIconToPng(options.name, 64, 64, outputRoot, '-64.png');
+    convertSingleIconToPng(options.name, outputSize, outputSize, outputRoot, `-${outputSize}.png`);
   }
 } else {
   fs.readdir(baseIconPath, function (err, files) {
@@ -109,9 +150,7 @@ if (options.name) {
         if (options.outputPlatform === 'ios') {
           convertIconToXcodeImageSet(iconName, outputRoot);
         } else {
-          convertSingleIconToPng(iconName, 16, 16, outputRoot, '-16.png');
-          convertSingleIconToPng(iconName, 32, 32, outputRoot, '-32.png');
-          convertSingleIconToPng(iconName, 64, 64, outputRoot, '-64.png');
+          convertSingleIconToPng(iconName, outputSize, outputSize, outputRoot, `-${outputSize}.png`);
         }
       }
     });
