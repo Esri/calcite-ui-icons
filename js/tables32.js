aa928c2eaa0440c38d248b81a477040efb5e1a84
johnmgriffith <jgriffith@esri.com>
2020-06-09 10:38:00 -0700

    updated tables icon

--- js/tables32.js
+++ js/tables32.js
@@ -1,1 +1,1 @@
-export const tables32 = "M31 12v18H9v-4h1v3h20V13h-2v-1zm-4 13V7h-3v1h2v16H6v-3H5v4zM1 20V2h22v18zm12-1v-3H6v3zm0-11H6v3h7zm-7 4v3h7v-3zm16 4h-8v3h8zm0-4h-8v3h8zm0-4h-8v3h8zM2 7h20V3H2zm0 4h3V8H2zm0 4h3v-3H2zm3 4v-3H2v3z";
+export const tables32 = "M31 12v18H9v-4h1v3h20V13h-2v-1zm-4 13V7h-3v1h2v16H6v-3H5v4zM1 20V2h22v18zm13-1v-3H7v3zm0-11H7v3h7zm-7 4v3h7v-3zm15 4h-7v3h7zm0-4h-7v3h7zm0-4h-7v3h7zM2 7h20V3H2zm0 4h4V8H2zm0 4h4v-3H2zm4 4v-3H2v3z";
